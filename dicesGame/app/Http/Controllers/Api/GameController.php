f0d7ea008d73460c6132dc174a131dc6deaf5493
Gabriela Maureira <ellamaureira@MacBook-Pro-de-Gabriela.local>
2023-04-23 13:13:53 +0200

    Get players & games

--- dicesGame/app/Http/Controllers/Api/GameController.php
+++ dicesGame/app/Http/Controllers/Api/GameController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api;
 
+use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreGameRequest;
 use App\Http\Requests\UpdateGameRequest;
 use App\Models\Game;
@@ -9,13 +10,13 @@ use App\Models\Game;
 class GameController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Games list for a specific player
      */
-    public function index()
+    public function index($id)
     {
-        //
+        return response()->json(['games' => Game::where('user_id', $id)->get()], 200);
     }
-
+    
     /**
      * Show the form for creating a new resource.
      */
@@ -59,8 +60,9 @@ class GameController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Game $game)
+    public function destroy(User $user)
     {
-        //
+        $user->games()->delete();
+        return response()->json(['message' => 'All delete succesfully'], 204);
     }
 }
--- dicesGame/routes/api.php
+++ dicesGame/routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\GameController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +19,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+
+// Players list
+Route::get('/players', [UserController::class, 'index'])->name('players.index');
+
+// Games list for a specific player
+Route::get('/players/{id}/games', [GameController::class, 'index'])->name('games.index');
+
