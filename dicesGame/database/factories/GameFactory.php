8d92c32453e14e6bce07fd641deb88134d97755c
Gabriela Maureira <ellamaureira@MacBook-Pro-de-Gabriela.local>
2023-04-23 11:59:30 +0200

    Database seeding

--- dicesGame/database/factories/GameFactory.php
+++ dicesGame/database/factories/GameFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\User;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Game>
@@ -16,8 +17,18 @@ class GameFactory extends Factory
      */
     public function definition(): array
     {
+        $dice_1 = fake()->numberBetween(1,6);
+        $dice_2 = fake()->numberBetween(1,6);
+        $result = $dice_1 + $dice_2 === 7;
+        
         return [
-            //
+            'user_id' => User::all()->random()->id,
+            'dice_1' => $dice_1,
+            'dice_2' => $dice_2,
+            'is_win' => $result,
+            
         ];
     }
 }
+
+
--- dicesGame/database/seeders/DatabaseSeeder.php
+++ dicesGame/database/seeders/DatabaseSeeder.php
@@ -12,11 +12,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        //\App\Models\User::factory(5)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        \App\Models\User::factory(10)
+            ->has(\App\Models\Game::factory()->count(5))
+            ->create();
+        
     }
 }
+
+
+    
