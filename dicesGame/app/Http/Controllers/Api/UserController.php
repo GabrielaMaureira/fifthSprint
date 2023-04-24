79e012e06fde9813a38f4c6142ca4869cb9048ad
Gabriela Maureira <ellamaureira@MacBook-Pro-de-Gabriela.local>
2023-04-24 03:38:18 +0200

    Registration working

--- dicesGame/app/Http/Controllers/Api/UserController.php
+++ dicesGame/app/Http/Controllers/Api/UserController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
 
 class UserController extends Controller
 {
@@ -17,12 +20,36 @@ class UserController extends Controller
     }
 
     /**
-     * Create new user
+     * Login for authenticated user
      */
-    public function store(Request $request, $id)
+    public function login(Request $request)
     {
         
     }
+
+    /**
+     * Register
+     */
+    public function register(Request $request)
+    {
+       $request->validate([
+            'name' => 'max:255|unique:users',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6'
+       ]);
+
+       $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+       ]);
+
+       $token = $user->createToken('auth_token')->accessToken;
+
+       return response()->json(['user' => $user, 'auth_token' => $token], 201);
+    }
+
+    
     /**
      * Name modified for a specific player
      */
--- dicesGame/routes/api.php
+++ dicesGame/routes/api.php
@@ -21,39 +21,39 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::controller(UserController::class)->group(function(){
+
+
+    // Login
+    //Route::post('login', 'login')->name('user.login');
+    
+    // Register
+    Route::post('/register', [UserController::class, 'register'])->name('user.register');
+
 
     // Players list & success rate
-    Route::get('/players', 'index')->name('players.index');
+    Route::get('/players', [UserController::class, 'index'])->name('players.index');
 
     // Name modified for a specific player
-    Route::patch('/players/{id}', 'update')->name('players.update');
+    Route::patch('/players/{id}', [UserController::class, 'update'])->name('players.update');
 
 
-});
-
-Route::controller(GameController::class)->group(function(){
-
+Route::middleware('auth:api')->group(function(){
     // Games list for a specific player
-    Route::get('players/{id}/games', 'index')->name('games.index');
+    Route::get('players/{id}/games', [GameController::class, 'index'])->name('games.index');
 
     // A specific player throw the dice
-    Route::post('/players/{id}/games', 'throwTheDice')->name('games.throwTheDice');
+    Route::post('/players/{id}/games', [GameController::class, 'throwTheDice'])->name('games.throwTheDice');
 
     // A specific player delete all the games
-    Route::delete('/players/{id}/games', 'destroy')->name('games.destroy');
+    Route::delete('/players/{id}/games', [GameController::class, 'destroy'])->name('games.destroy');
 
 });
-
-Route::controller(RankingController::class)->group(function(){
 
     // Average success rate of all players
-    Route::get('/players/ranking', 'index')->name('ranking.index');
+    Route::get('/players/ranking', [RankingController::class, 'index'])->name('ranking.index');
     
     // Highest success rate
-    Route::get('/players/ranking/winner', 'winner')->name('ranking.winner');
+    Route::get('/players/ranking/winner', [RankingController::class, 'winner'])->name('ranking.winner');
 
     // Lowest success rate
-    Route::get('/players/ranking/loser', 'loser')->name('ranking.loser');
-
-});
+    Route::get('/players/ranking/loser', [RankingController::class, 'loser'])->name('ranking.loser');
