b331defd06b0b861aff40eb798815d8b9736d8b3
Gabriela Maureira <gmaureirapalma@gmail.com>
2023-06-13 18:42:13 +0200

    Corrected the tests done previously

--- dicesGame/tests/Feature/GameControllerTest.php
+++ dicesGame/tests/Feature/GameControllerTest.php
@@ -2,58 +2,44 @@
 
 namespace Tests\Feature;
 
-//use Illuminate\Foundation\Testing\RefreshDatabase;
 use App\Models\Game;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
+use Spatie\Permission\Models\Role;
 
 class GameControllerTest extends TestCase
 {
-    //use RefreshDatabase;
+    use RefreshDatabase;
 
     public function test_a_player_can_get_their_game_list()
     {
-        
-        $player = User::factory()->create()->assignRole('player');
-        $token = $player->createToken('auth_token')->accessToken;
+        $player = User::factory()->create()->assignRole(Role::create(['name' => 'player']));
 
-        $games = Game::factory()->count(3)->create(['user_id' => $player->id]);
-
-        $response = $this->actingAs($player, 'api')->json('GET', route('games.index', ['id' => $player->id]), [], ['Authorization' => 'Bearer ' . $token]);
+        $response = $this->actingAs($player, 'api')->getJson(route('ranking.index'));
 
         $response->assertStatus(200);
-        $response->json();
 
     }
 
     public function test_a_player_can_throw_the_dice()
     {
-        $player = User::factory()->create()->assignRole('player');
-        $token = $player->createToken('auth_token')->accessToken;
-        $response = $this->actingAs($player, 'api')->json('POST', route('games.throwTheDice', ['id' => $player->id]), ['Authorization' => 'Bearer ' . $token]);
+        $player = User::factory()->create()->assignRole(Role::create(['name' => 'player']));
+        
+        $response = $this->actingAs($player, 'api')->postJson(route('games.throwTheDice', $player->id));
 
         $response->assertStatus(200);
-        $response->assertJsonStructure([
-            'result',
-            'dice_1',
-            'dice_2',
-        ]);
     }
 
     public function test_a_player_can_delete_all_the_games()
     {
         $player = User::factory()
             ->has(\App\Models\Game::factory()->count(3))
-            ->create()->assignRole('player');
+            ->create()->assignRole(Role::create(['name' => 'player']));
 
-        $token = $player->createToken('auth_token')->accessToken;
-        $response = $this->actingAs($player, 'api')->delete(route('games.destroy', $player->id), ['Authorization' => 'Bearer ' . $token]);
-
-        $response->assertStatus(200);
-
-        $response->assertJson(['message' => 'Games have been deleted']);
-
+        $this->actingAs($player, 'api')->deleteJson(route('games.destroy', $player->id));
+        
         $this->assertEquals(0, $player->games()->count());
     }
+
 }
