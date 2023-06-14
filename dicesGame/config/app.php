94c22925d56e53a2fd4fd9cadaf58559058ca056
Gabriela Maureira <gmaureirapalma@gmail.com>
2023-06-14 17:13:11 +0200

    Fixing errors

--- dicesGame/config/app.php
+++ dicesGame/config/app.php
@@ -171,6 +171,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         Spatie\Permission\PermissionServiceProvider::class
+        
     ])->toArray(),
 
     /*
